fc1abd78e6e13b1b0e2ca452fb6475e60647f8aa
Jarod Gilliam <jarodsimongilliam@gmail.com>
2024-09-07 06:06:10 -0400

    Seporates finished game and blank file

---
+++ Interview Game/finished_game.py
@@ -0,0 +1,21 @@
+from interviewprint import print_character_dialogue, print_player_options
+
+print_character_dialogue("what.", character_name="Amy")
+print_player_options("HI how are you?")
+print_player_options("WHAT!")
+print_player_options("Go away.")
+
+user_input = input()
+
+if user_input == "1":
+    print_character_dialogue("I'm fine.")
+elif user_input == "2":
+    print_character_dialogue("Was 2")
+else:
+    print_character_dialogue("That was rude.", mood="angry")
+
+print_character_dialogue("Goodbye")
+
+# visual novel
+# interactive fiction
+# text adventure
